0b257e72c376537f8e820245d63acff8aefb2435
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-02 22:55:01 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -13,27 +13,38 @@ def play(songs)
    puts "Please enter a song name or number:"
    user_respond = gets.chomp
    user_respond.strip
-   binding.pry
-   if user_respond == songs ||  user_respond == idx
-     puts "Playing #{songs[0]}"
-   else
-     "Invalid input, please try again"
+   #binding.pry
+   if user_respond.to_i == 0 
+     if songs.include?(user_respond)
+     puts "Playing #{user_respond}"
+     else
+      puts "Invalid input, please try again"
+     end
+     else
+       if songs[user_respond.to_i-1]
+         puts "Playing #{songs[user_respond.to_i-1]}"
+       else
+       puts "Invalid input, please try again"
+      end
    end
 end
 
 def list (songs)
-   songs = ['.Phoenix - 1901','.Tokyo Police Club - Wait Up','. Sufjan Stevens - Too Much','., The Naked and the Famous - Young Blood','. (Far From) Home - Tiga','.The Cults - Abducted','.Phoenix - Consolation Prizes','.Harry Chapin - Cats in the Cradle','. Amos Lee - Keep It Loose, Keep It Tight']
-   split.songs.each_with_index do |word ,idx|
-     
+   #songs = ['.Phoenix - 1901','.Tokyo Police Club - Wait Up','. Sufjan Stevens - Too Much','., The Naked and the Famous - Young Blood','. (Far From) Home - Tiga','.The Cults - Abducted','.Phoenix - Consolation Prizes','.Harry Chapin - Cats in the Cradle','. Amos Lee - Keep It Loose, Keep It Tight']
+     songs.each_with_index do |word ,idx|
+     #binding.pry
+   
+   puts "#{idx+1}. #{word}"
    end
-   return (idx+1.to_s) + word
 end
 
-def exit_junkebox
+def exit_jukebox
    puts "Goodbye"
 end
 
-def run 
+def run(songs)
+     puts "Please enter a command:"
+      user_respond = gets.strip 
    if user_respond == 'help'
      help
    elsif
@@ -44,8 +55,10 @@ def run
      play(songs)
    else
     user_respond == 'exit'
-     exit_junkebox
+     
+     exit_jukebox
       
     
    end
 end
+   

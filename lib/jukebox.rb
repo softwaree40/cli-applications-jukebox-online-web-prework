59aa874b0e3ba97910882983706ea06b6abda4fc
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-02 05:51:53 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,11 +1,11 @@
 # Add your code here
 def help
   #puts '- Please enter a command: '
-  puts 'I accept the following commands:'
-  puts '- help : displays this help message'
-  puts '- list : displays a list of songs you can play'
-  puts '- play : lets you choose a song to play'
-  puts '- exit : exits this program'
+  puts "I accept the following commands:"
+  puts "- help:displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
   
 end
 def play(songs)

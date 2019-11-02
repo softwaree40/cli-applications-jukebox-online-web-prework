b05f210a1e08487630cd0338f212742fb8eba060
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-02 22:20:19 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,9 +32,10 @@ end
 def list (songs)
    #songs = ['.Phoenix - 1901','.Tokyo Police Club - Wait Up','. Sufjan Stevens - Too Much','., The Naked and the Famous - Young Blood','. (Far From) Home - Tiga','.The Cults - Abducted','.Phoenix - Consolation Prizes','.Harry Chapin - Cats in the Cradle','. Amos Lee - Keep It Loose, Keep It Tight']
      songs.each_with_index do |word ,idx|
-     binding.pry
+     #binding.pry
    end
    return (idx+1.to_s) + word
+   binding.pry
 end
 
 def exit_junkebox
